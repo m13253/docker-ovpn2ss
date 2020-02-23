24f21f9848511cb5f89504d0869493cba0134c73
Star Brilliant <coder@poorlab.com>
2020-02-24 00:20:13 +0800

    Switch to nftables

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM alpine:edge
 
 RUN apk update && \
     apk upgrade && \
-    apk add build-base openvpn git go supervisor && \
+    apk add build-base nftables openvpn git go supervisor && \
     go get -u -v github.com/shadowsocks/go-shadowsocks2
 
 ADD entrypoint.sh resolv-vpn.conf resolv.conf route-pre-down.sh route-up.sh start-ss.sh /
--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,8 @@ GATEWAY_IPv4="$(ip -4 route get 192.0.2.1 dev eth0 | grep -o 'via\s\S*')"
 GATEWAY_IPv6="$(ip -6 route get 2001:db8::1 dev eth0 | grep -o 'via\s\S*')"
 
 # (Optionally) disable IPv6 if the VPN provider does not have IPv6 connectivity
-#ip -6 addr flush dev eth0
-#ip -6 route flush all
+#ip -6 route flush table main
+
 if [ -n "$GATEWAY_IPv4" ]
 then
     ip -4 route add default $GATEWAY_IPv4 dev eth0 table 8388
@@ -16,12 +16,21 @@ then
     ip -4 route add default $GATEWAY_IPv6 dev eth0 table 8388
     ip -6 rule add fwmark 0x8388 table 8388 priority 8388
 fi
-iptables -t mangle -A INPUT -i eth0 -j MARK --set-mark 0x8388
-iptables -t mangle -A INPUT -j CONNMARK --save-mark
-iptables -t mangle -A OUTPUT -j CONNMARK --restore-mark
-ip6tables -t mangle -A INPUT -i eth0 -j MARK --set-mark 0x8388
-ip6tables -t mangle -A INPUT -j CONNMARK --save-mark
-ip6tables -t mangle -A OUTPUT -j CONNMARK --restore-mark
+
+nft add table inet mangle
+nft add chain inet mangle input '{' type filter hook input priority mangle ';' '}'
+nft add chain inet mangle output '{' type route hook output priority mangle ';' policy accept ';' '}'
+nft add rule inet mangle input iifname "eth0" counter meta mark set 0x8388
+nft add rule inet mangle input counter ct mark set mark
+nft add rule inet mangle output counter meta mark set ct mark
+
+nft add table ip nat
+nft add chain ip nat prerouting '{' type nat hook prerouting priority dstnat ';' policy accept ';' '}'
+nft add rule ip nat prerouting iifname "eth0" meta l4proto udp fib daddr type local counter redirect
+
+nft add table ip6 nat
+nft add chain ip6 nat prerouting '{' type nat hook prerouting priority dstnat ';' policy accept ';' '}'
+nft add rule ip6 nat prerouting iifname "eth0" meta l4proto udp fib daddr type local counter redirect
 
 umount /etc/resolv.conf
 cp /resolv.conf /etc/resolv.conf
